53d90dde57714f311bf7a366900438cbdfd094bb
lexycf <43943650+lexycf@users.noreply.github.com>
2019-01-21 10:13:23 +0800

    update

--- invite/js/regissuccess.js
+++ invite/js/regissuccess.js
@@ -1,14 +1,14 @@
-var func={
-    node:{
-        invitebtn:'.inviteBtn',
-        hidebox:'.hideBox',
-        close:'.close'
+var func = {
+    node: {
+        invitebtn: '.inviteBtn',
+        hidebox: '.hideBox',
+        close: '.close'
     },
-    bindEvent:function(){
-        $(func.node.invitebtn).click(function(){
+    bindEvent: function () {
+        $(func.node.invitebtn).click(function () {
             $(func.node.hidebox).show();
         })
-        $(func.node.close).click(function(){
+        $(func.node.close).click(function () {
             $(func.node.hidebox).hide();
         })
     }
